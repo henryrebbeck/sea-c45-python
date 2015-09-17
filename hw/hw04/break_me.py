ef7b82b01b9d375b9ab5872bb584ee1bf6a18d9a
Henry Rebbeck <henry.rebbeck@gmail.com>
2015-09-17 10:47:25 -0700

    updates to breakme

---
+++ hw/hw04/break_me.py
@@ -0,0 +1,16 @@
+def exhibit_name_error():
+    return 4 + undefined_name_produces_name_error * 3
+    # Replace this line with something that exhibits a name exhibit_name_error
+    # in this case jack is not defined so it produces a name error
+
+
+def exhibit_attribute_error():
+    return str.unknown_attribute_produces_attribute_error
+    # replace this line with something that exhibits
+    # an exhibit_attribute_error
+
+
+def exhibit_type_error():
+    return 1 + 'concatenating a int to a str produces a type_error'
+    # a new scope with indented lines
+    # Example  where you cannot concatenate 'str' and 'int' objects
