4fdd451b6cc3cf636bf9a50a104f8cf90a2ec88a
Henry Rebbeck <henry.rebbeck@gmail.com>
2015-09-18 12:33:56 -0700

    final changes to hw04

---
+++ hw/hw04/break_me.py
@@ -0,0 +1,17 @@
+def exhibit_name_error():
+    4 + undefined_name_produces_name_error * 3
+    # Replace this line with something that exhibits a name exhibit_name_error
+    # in this case undefined_name_produces_name_error is not defined
+    # so it produces a name error
+
+
+def exhibit_attribute_error():
+    str.unknown_attribute_produces_attribute_error
+    # replace this line with something that exhibits
+    # an exhibit_attribute_error
+
+
+def exhibit_type_error():
+    return 1 + 'concatenating a int to a str produces a type_error'
+    # a new scope with indented lines
+    # Example  where you cannot concatenate 'str' and 'int' objects
