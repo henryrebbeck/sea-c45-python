cfedd5dec877bc61aa656f2f9fe56e4212e7c8df
Henry Rebbeck <henry.rebbeck@gmail.com>
2015-10-15 13:07:03 -0700

    Stage 1 and stage 2 still pretty messy code

--- hw/hw18/html_render.py
+++ hw/hw18/html_render.py
@@ -2,72 +2,97 @@
 
 """
 Python class example.
+
 """
 
 # The start of it all:
 # Fill it all in here.
 
-# indent: start with just x amount of spaces, then use string formatting
-# justify right {:>x}
 
-TAB_SIZE = 4
-
-
+#  class Element(object):
 class Element(object):
-    def __init__(self, content='', indent=0, **attrs):
-        self.tag = ''
-        self.indent = indent
-        self.children_indent = indent + TAB_SIZE
-        self.content = ''
-        self.children = []
-        self.parent = None
-        self.append(content)
+    "doc string :Create Element class to produce HTML"
 
-    #def __str__(self):
-     #   return self.render()
+    tag = ''
+    indent = '   '
 
-    def render(self, f):
-        f.write(self.render_html())
+    def __init__(self, content=None, **attributes):
+        if content is not None:
+            self.content = [str(content)]
+        else:
+            self.content = []
+        self.attributes = attributes
+        print('attributes in __init__:', attributes)
+
+    def append(self, new_content):
+
+        """ Add some new content to the element
+        """
+        self.content.append(new_content)
+
+    def render(self, file_new, ind=""):
+        """Render the content to the given file like object"""
+        # Start a new line and print out the tag
+        file_new.write("\n")
+        file_new.write(ind)
+        file_new.write("<%s>" % self.tag)
+
+        '''
+        print(r"\n,ind,self.tag from inside render :","\n",ind,self.tag)
+        print('self.attributes.itesm', self.attributes.items())
+        for key, value in self.attributes.items():
+            print('2..key and value in self.attributes.itesm',key,value,self.attributes.items())
+            file_new.write(' %s="%s"' % (key, value))
+        file_new.write(">")'''
+
+        print('Loop through self.content :', self.content, '\n')
+        for s in self.content:  # loop through the content of the class
+            # We have defined the content as either a string
+            # or an empty list(element??).
+            print("s in self.content: s is---->", s)
+            if isinstance(s, Element):  # this line is key
+                # this says if the item s is of class Element then
+                # send it back to the renderer otherwise its a string
+                # so print/render it
+                print('if isinstance (s,Element)  s:', s, 'Element', Element)
+                # this is the recursive render call where we send the element
+                # to the renderer again, but making the indent have an
+                # additional indent of itself.
+                s.render(file_new, ind + self.indent)
+            else:
+                file_new.write("\n" + ind + self.indent + str(s))
+        file_new.write("\n" + ind + "</" + self.tag + ">")
+
+        #  def render(self, f):
+        #  f.write(self.render_html())
 
     def render_html(self):
         return u'\n{i}<{t}>{c}{ch}\n{i}</>'.format(
             i=' ' * self.indent,
             t=self.tag,
             c=self.content,
-            ch=''.join([child.render_html() for child in self.children]))
-
-    def format_content(self, content):
-        return '\n{}{}'.format(' ' * self.children_indent, content)
-
-    def append(self, child=None):
-        if child:
-            if isinstance(child, Element):
-                self.children.append(child)
-                child.indent = self.children_indent
-                child.parent = self
-            elif isinstance(child, str) or isinstance(child, unicode):
-                self.content = ''.join(
-                    [self.content, self.format_content(child)])
-            else:
-                pass
-                ''' raise TypeError('Object appended to Element must be a string'
-                                'or another Element; got {} instead'
-                                .format(type(child)))'''
+            ch=''.join([child.render_html() for child in self]))
 
 
 class Html(Element):
-    def __init__(self, *args, **kwargs):
-        super(Html, self).__init__(*args, **kwargs)
-        self.tag = 'html'
+    "Create a subclass element to produce a html tagged element (page)"
+    tag = 'Html'
+    indent = '    '
+    # This indentation is for the next line of text
+    # (ie the body tag if thats called next)
+    print('Html', indent, tag)
 
 
 class Body(Element):
-    def __init__(self, *args, **kwargs):
-        super(Body, self).__init__(*args, **kwargs)
-        self.tag = 'body'
+    "Create a class element to produce a body element"
+    " (which contains the page element (which contains the paragraph element))"
+    tag = 'body'
+    indent = '    '
+    print('Body', indent, tag)
 
 
 class P(Element):
-    def __init__(self, *args, **kwargs):
-        super(P, self).__init__(*args, **kwargs)
-        self.tag = 'p'
+    "docstring: Create a subclass element to produce a paragraph element"
+    tag = 'p'
+    indent = '    '
+    print('p', indent, tag)
